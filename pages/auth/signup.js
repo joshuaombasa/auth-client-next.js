ff5b01ac52cebb4e10c6b88c517fef1990830c55
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-05-23 22:03:34 +0300

    initial commit

---
+++ pages/auth/signup.js
@@ -0,0 +1,60 @@
+import axios from 'axios'
+import Router from 'next/router'
+import React, { useState } from 'react'
+
+export default function Signup() {
+
+  const [formData, setFormData] = useState({
+    email: '',
+    password: ''
+  })
+
+  const [errors, setErrors] = useState(null)
+
+  function handleChange(event) {
+    const { name, value } = event.target
+
+    setFormData(prevState => ({
+      ...prevState,
+      [name]: value
+    }))
+  }
+
+  async function handleSubmit(event) {
+    event.preventDefault()
+
+    try {
+      setErrors(null)
+      const res = await axios.post('http://localhost:4000/api/users/signup', {
+        email: formData.email,
+        password: formData.password
+      })
+      
+      setFormData({email: '', password: ''})
+      Router.push('/')
+    } catch (error) {
+      setErrors(error.response.data.errors)
+    }
+  
+
+  }
+
+  return (
+    <div className='common-width'>
+      <form className='signup-form' onSubmit={handleSubmit}>
+        <h1>Sign Up</h1>
+        <div>
+          <label htmlFor='email'>Password</label>
+          <input name='email' id='email' value={formData.email} onChange={handleChange} />
+        </div>
+        <div>
+          <label htmlFor='password'>Password</label>
+          <input name='password' id='password' value={formData.password} onChange={handleChange} />
+        </div>
+        {errors && errors.map(error => <span key={error.message} className="error-message">{error.message}</span>)}
+        <button className='btn-style'>Sign up</button>
+
+      </form>
+    </div>
+  )
+}
